15aafe4bc6f575f2bb1fe318db49320212bbba10
reihandany <166046605+reihandany@users.noreply.github.com>
2024-07-04 22:09:47 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        rubah fox = new rubah();
+        fox.Name();
+        fox.Color();
+        fox.Gender();
+        fox.Age();
+        fox.Food();
+        System.out.println("  ");
+        fox.Picture();
+    }
+}
---
+++ src/rubah.java
@@ -0,0 +1,41 @@
+public class rubah {
+        public String name = "Snorlax";
+        public String color = "grey";
+        public String gender = "laki-Laki";
+        public int age = 3;
+        public String food = "meat";
+
+        public void Name(){
+                System.out.println("Saya mempunyai seekor rubah bernama"+this.name);
+        }
+
+        public void Color(){
+                System.out.println(this.name+" berwarna "+this.color);
+        }
+
+        public void Gender(){
+                System.out.println(this.name+" berkelamin "+this.gender);
+        }
+
+        public void Age(){
+                System.out.println(this.name+" berumur "+this.age+"tahun");
+        }
+
+        public void Food(){
+                System.out.println(this.name+" suka makan "+this.food);
+        }
+        public void Picture(){
+                System.out.println("       /\\         /\\   ");
+                System.out.println("      /  \\_______/  \\  ");
+                System.out.println("     /                \\ ");
+                System.out.println("   /    /0\\      /0\\    \\ ");
+                System.out.println("  /                      \\ ");
+                System.out.println(" \\           /o\\          / ");
+                System.out.println("   \\                    / ");
+                System.out.println("     \\       UU       / ");
+                System.out.println("       \\            / ");
+                System.out.println("          \\ ~~~~~ /  ");
+        }
+}
+
+
